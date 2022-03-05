e75b790d7b3cc4819d95f5ee85a47f6557257aca
Florian Romann thinkpad_fedora <s8flroma@stud.uni-saarland.de>
2022-03-05 22:50:53 +0100

    added libs, ready to be run as an experiment and interesting still to be validated

---
+++ results/experiment_ready.py
@@ -0,0 +1,72 @@
+experiment_ready = {
+
+    'libldac': (['libldac_encode_fuzzer'], '2022-01-28T17:47:00+00:00', [
+        'RIFF (little-endian) data, WAVE audio, Microsoft PCM, 8 bit, mono 11025 Hz'
+        'RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, stereo 48000 Hz'
+        'RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 11025 Hz'
+        'RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 44100 Hz'
+        'RIFF (little-endian) data, WAVE audio, Microsoft PCM, 8 bit, mono 16000 Hz'
+        'RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 48000 Hz'
+        'RIFF (little-endian) data, WAVE audio, ITU G.711 mu-law, stereo 8000 Hz'
+        'RIFF (little-endian) data, WAVE audio, Microsoft PCM, 8 bit, mono 44100 Hz'
+        'RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, 4 channels 8000 Hz'
+        'RIFF (little-endian) data, WAVE audio, ITU G.711 A-law, stereo 8000 Hz'
+        'RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, stereo 44100 Hz'
+        'RIFF (little-endian) data, WAVE audio, Microsoft ADPCM, mono 11025 Hz'
+        'RIFF (little-endian) data, WAVE audio, GSM 6.10, mono 8000 Hz'
+        'RIFF (little-endian) data, WAVE audio, stereo 8000 Hz'
+        'RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 1000 Hz'
+        'ASCII text'
+    ]),
+
+    'giflib': ('dgif_target', '2020-07-06T13:18:23-07:00', [
+        'GIF image data, version 89a, 125 x 25'
+        'GIF image data, version 89a, 290 x 48'
+        'GIF image data, version 89a, 275 x 100'
+        'GIF image data, version 89a, 30 x 60'
+        'GIF image data, version 89a, 500 x 220'
+        'GIF image data, version 89a, 350 x 230'
+        'GIF image data, version 87a, 40 x 40'
+        'GIF image data, version 89a, 400 x 160'
+        'GIF image data, version 87a, 640 x 350'
+        'GIF image data, version 87a, 320 x 200'
+        'GIF image data, version 89a, 340 x 150'
+        'GIF image data, version 89a, 10 x 10'
+        'GIF image data, version 89a, 33 x 87'
+        'GIF image data, version 89a, 400 x 240'
+        'GIF image data, version 89a, 11 x 29'
+        'GIF image data, version 89a, 140 x 90'
+        'GIF image data, version 89a, 700 x 220'
+        'GIF image data, version 89a, 100 x 100'
+        'GIF image data, version 87a, 640 x 400'
+        'GIF image data, version 89a, 370 x 110'
+    ]),
+
+    'libwebp':
+        ([
+             'fuzz_simple_api',
+             'fuzz_advanced_api',
+             'fuzz_animation_api',
+             'fuzz_demux_api'],
+         '2022-01-21T17:24:29+00:00', [
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 63x66, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 128x128, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 1x1, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 31x13, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 20x16, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 1920x96, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 175x143, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 1x13, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 352x288, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 176x144, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 95x95, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 160x160, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 13x1, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 176x144, Scaling:, bad color specification, no clamping required'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 200x200, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 96x96, Scaling: [none]x[none], YUV color, decoders should clamp'
+             'RIFF (little-endian) data, Web/P image, VP8 encoding, 320x240, Scaling: [none]x[none], YUV color, decoders should clamp'
+         ]),
+
+}
